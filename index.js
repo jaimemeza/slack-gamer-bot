44376f2dd8f2116ef73c48e9c3bfb518a6547fa9
Jaime Meza <hello@jaimemeza.com>
2018-10-02 21:16:26 -0500

    Check vertical win

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ let game = 0;
 let user1 = '';
 let user2 = '';
 let activeUser = '';
+let count = 0;
 let hi = ['hi', 'hello', 'hola', 'que tal', 'morning']
 rtm.start();
 rtm.on("message", msg => {
@@ -22,12 +23,16 @@ rtm.on("message", msg => {
     // Convert the message to lowercase
     msg.text = msg.text.toLowerCase()
   
-    if (hi.indexOf(msg.text) != -1) {
-        send("Hi <@" + msg.user + ">", msg.channel);
-    } else if (msg.text.slice(0,11) == "how are you") {
-        send("Doing well and you <@" + msg.user + ">?",msg.channel);
-    } else if ( msg.text == "say hello" && msg.user == "UB5FHHXPV") {
+    if (hi.indexOf(msg.text) != -1 && msg.user == 'UB5FHHXPV') {
+        send("Hi Master.", msg.channel);
+    } else if (hi.indexOf(msg.text) != -1) {
+        send("Hi, <@" + msg.user + ">",msg.channel);
+    } else if ( msg.text == "bye" && msg.user == "UB5FHHXPV") {
+        send("Bye Grand Master!!", msg.channel);
+    }  else if ( msg.text == "say hello" && msg.user == "UB5FHHXPV") {
         send("Hello <!channel>", msg.channel);
+    }  else if ( msg.text.slice(msg.text.length - 6 ,msg.text.length) == "ready?") {
+        send("Ready for what <@" + msg.user + ">?", msg.channel);
     }  else if ( msg.text.slice(msg.text.length - 2, msg.text.length) == "??") {
         send("What's wrong with you <@" + msg.user + ">?", msg.channel);
     } else if ( msg.text.charAt(msg.text.length - 1) == "?") {
@@ -36,15 +41,20 @@ rtm.on("message", msg => {
         send("Yeah what <@" + msg.user + ">?", msg.channel);
     } else if ( msg.text.slice(0,4) == "drop") {
         send("Deleting all the feelings for <@" + msg.user + ">", msg.channel);
+    } else if ( msg.text.slice(0,7) == "y ahora") {
+        send("Yo juego contigo, a que quieres jugar <@" + msg.user + ">?", msg.channel);
     } else if ( msg.text.slice(0,4) == "jaja") {
         send("Mucha risa <@" + msg.user + ">?", msg.channel);
     } else if ( msg.text == "grr" && msg.user == "UB5FHHXPV") {
         send("GGGGGRRRRRRRRR!!!!!!!!", msg.channel);
+    } else if ( msg.text == '!status' ) {
+        let temp = game == 1 ? 'on'  : 'off';
+        send ('The game status is: ' + temp, msg.channel);
     } else if ( msg.text == "start game" && game == 0) {
         game = 1;
         send('Let the games begin, who want to play tic tac toe? (Responde: me)', msg.channel);  
     }
-    game == 1 && msg.user == 'UB5FHHXPV' && msg.text == "end game" ? endGame() : null;
+    game == 1 && msg.user == 'UB5FHHXPV' && msg.text == "end game" ? endGame(msg.channel) : null;
     game == 1 && user1 != '' && user2 != '' && activeUser == msg.user ? updateGame(msg.text.charAt(0), msg.text.charAt(1), msg.channel) : null;
     game == 1 && (user1 == '' || user2 == '') ? addPlayer(msg.text, msg.user, msg.channel) : null;
 });
@@ -53,27 +63,30 @@ send = (info, channel) => {
     rtm
       .sendMessage(info, channel)
       // Returns a promise that resolves when the message is sent
-      .then(msg => {}
-     
+      .then(msg => {
+console.log('');
+      }
       )
       .catch(console.error);
   }
 
   someWin = () => {
-    if ((a[0] == a[1] && a[1] == a[2] && a[0] != '   ') || (c[0] == c[1] && c[1] == c[2] && c[0] != '   ') || (((a[0] == b[1] && b[1] == c[2]) || (a[2] == b[1] && b[1] == c[0]) || (b[0] == b[1] && b[1] == b[2])) && b[1] != '   ')) {
+    if ((a[0] == a[1] && a[1] == a[2] && a[0] != '   ') || (c[0] == c[1] && c[1] == c[2] && c[0] != '   ') || (((a[0] == b[1] && b[1] == c[2]) || (a[2] == b[1] && b[1] == c[0]) || (b[0] == b[1] && b[1] == b[2])) && b[1] != '   ') || (a[0] == b[0] && c[0] == b[0] && c[0] != '   ') || (a[1] == b[1] && c[1] == b[1] && c[1] != '   ') || (a[2] == b[2] && c[2] == b[2] && c[2] != '   ') ) {
         return activeUser;
     } else {
         return false;
     }
 }
 
-endGame = () => {
+endGame = (channel) => {
     a = ['   ', '   ', '   '];
     b = ['   ', '   ', '   '];
     c = ['   ', '   ', '   '];
     game = 0;
     user1 = '';
     user2 = '';
+    count = 0;
+    send('The game has finish.',channel)
 }
 
 updateGame = (position, index,channel) => {
@@ -100,12 +113,18 @@ updateGame = (position, index,channel) => {
 }
 
 showResult = (channel) => {
-    send('A:  ' + a[0] + '|' + a[1] + '|' + a[2], channel);
+    send('A:  ' + a[0] + '|' + a[1] + '|' + a[2] + '\n', channel);
     send('B:  ' + b[0] + '|' + b[1] + '|' + b[2], channel);
     send('C:  ' + c[0] + '|' + c[1] + '|' + c[2], channel);
-    if (someWin() != false ) {
+    count++;
+    let winner = someWin();
+    console.log(winner);
+    if (winner != false ) {
         send('<@' + activeUser + '> Wins',channel);
-        return endGame();
+        return endGame(channel);
+    } else if (winner == false && count == 9) {
+        send("It's a tie",channel);
+        return endGame(channel);
     }
     activeUser = activeUser == user1 ? user2 :user1;
 }
@@ -113,16 +132,21 @@ showResult = (channel) => {
 addPlayer = (text, user, channel) => {
     if (game == 1) {
         // console.log(user1, user2)
-        if (text.toLowerCase() == 'me' && user1 == '') {
+        if (text == 'me' && user1 == '') {
             user1 = user;
             send('User1: <@' + user + '>', channel);
             activeUser = user1;
-        } else if (text.toLowerCase() == 'me' && user2 == '' && user1 != '' && user1 != user) {
+        } else if (text == 'me' && user2 == '' && user1 != '' && user1 != user) {
             user2 = user;
             send('User2: <@' + user + '>', channel);
             updateGame(channel);
-        } else if (text.toLowerCase() == 'me' && user1 == user && user1 != '' && user2 == '') {
+        } else if (text == 'me' && user1 == user && user1 != '' && user2 == '') {
             send('User1 must be different to User2', channel);
         }
     }
 }
+
+construct = () => {
+
+}
+
